ca88b6b42aece576ca29365e22ffe92385940846
nitipat21 <101240773+nitipat21@users.noreply.github.com>
2022-08-19 23:59:13 +1000

    Update palindrome-checker.js

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,5 +1,29 @@
 function palindrome(str) {
-  return true;
+
+  // remove all non-alphanumeric characters and convert to lower case
+  let forwardStr = str.replace(/[\W]|_/gi, "").toLowerCase();
+
+  // create array to store string in backward
+  let backwardArray = [];
+
+  // loop though forward string to store it in backward array
+  for (let i = 0; i < forwardStr.length; i++) {
+    backwardArray.unshift(forwardStr[i]);
+  }
+
+  // join string from backward array 
+  let backwardStr = backwardArray.join("");
+
+  // check if it is palindrome
+  if (forwardStr === backwardStr) {
+    return true
+  } else {
+    return false;
+  }
 }
 
 palindrome("eye");
+palindrome("_eye");
+palindrome("race car");
+palindrome("A man, a plan, a canal. Panama")
+palindrome("nope")
